d475f934b42c2044b3711b30da673929201218de
Cameron Childress <cameronc@sumoc.com>
2023-06-14 14:08:44 -0400

    wipwip

--- src/generators/ts/typescript-types.ts
+++ src/generators/ts/typescript-types.ts
@@ -1,6 +1,5 @@
 import { join } from "path";
 import { TypeScriptSource } from "./typescript-source";
-import { formatStringByNamingStrategy } from "../../core/naming-strategy";
 import { Service } from "../../core/service";
 
 export class TypescriptTypes extends TypeScriptSource {
@@ -14,20 +13,9 @@ export class TypescriptTypes extends TypeScriptSource {
   }
 
   preSynthesize(): void {
-    super.preSynthesize();
-
     /***************************************************************************
      * GENERIC TYPES
      **************************************************************************/
-    const responseType = formatStringByNamingStrategy(
-      "response",
-      this.service.namingStrategy.ts.typeName
-    );
-
-    const listResponseType = formatStringByNamingStrategy(
-      "list-response",
-      this.service.namingStrategy.ts.typeName
-    );
 
     this.open(`export type Error = {`);
     this.line(`code: number;`);
@@ -37,14 +25,14 @@ export class TypescriptTypes extends TypeScriptSource {
     this.close(`}`);
     this.line("\n");
 
-    this.open(`export type ${responseType}<T> = {`);
+    this.open(`export type ${this.service.tsResponseTypeName}<T> = {`);
     this.line(`data?: T;`);
     this.line(`errors: Error[];`);
     this.line(`status: number;`);
     this.close(`}`);
     this.line("\n");
 
-    this.open(`export type ${listResponseType}<T> = {`);
+    this.open(`export type ${this.service.tsLlistResponseTypeName}<T> = {`);
     this.line(`data?: T[];`);
     this.line(`errors: Error[];`);
     this.line(`status: number;`);
@@ -73,5 +61,6 @@ export class TypescriptTypes extends TypeScriptSource {
     /***************************************************************************
      * INPUTS / OUTPUTS - TODO
      **************************************************************************/
+    super.preSynthesize();
   }
 }
