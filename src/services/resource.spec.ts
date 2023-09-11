0bb6424d0337b1e5e47a5085a198ed954adfb2f5
Cameron Childress <cameronc@sumoc.com>
2023-09-11 12:09:33 -0400

    wip

--- src/services/resource.spec.ts
+++ src/services/resource.spec.ts
@@ -1,15 +1,29 @@
 import { Resource } from "./resource";
 import { testDataService } from "../util/test-util";
 
-test("Smoke test", () => {
-  const service = testDataService();
-  const resource = new Resource(service, { name: "foo" });
-  expect(resource).toBeTruthy();
+describe("success conditions", () => {
+  test("Smoke test", () => {
+    const service = testDataService();
+    const resource = new Resource(service, { name: "foo" });
+    expect(resource).toBeTruthy();
+  });
+
+  test("Able to add attribute", () => {
+    const service = testDataService();
+    const resource = new Resource(service, { name: "foo" });
+    const attribute = resource.addAttribute({ name: "bar" });
+    expect(attribute).toBeTruthy();
+  });
 });
 
-test("Able to add attribute", () => {
-  const service = testDataService();
-  const resource = new Resource(service, { name: "foo" });
-  const attribute = resource.addAttribute({ name: "bar" });
-  expect(attribute).toBeTruthy();
+describe("failure conditions", () => {
+  test("Duplicate resources not allowed", () => {
+    const service = testDataService();
+
+    new Resource(service, { name: "foo" });
+
+    expect(() => {
+      new Resource(service, { name: "foo" });
+    }).toThrow();
+  });
 });
