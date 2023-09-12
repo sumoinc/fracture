b79ca878571979ceb5028b5c7773034858bca024
Cameron Childress <cameronc@sumoc.com>
2023-09-12 08:58:46 -0400

    wip

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,9 +4,7 @@ import {
 } from "projen/lib/javascript";
 import { TypeScriptProject } from "projen/lib/typescript";
 import { NetlifyEnvironment } from "./src/environments/netlify-environment";
-import { KitchenSink } from "./src/generators/kitchen-sink";
 import { VsCodeConfiguration } from "./src/projen/vscode";
-import { DataService } from "./src/services/data-service";
 import { VitePressSite } from "./src/sites/vitepress/vitepress-site";
 
 const authorName = "Cameron Childress";
