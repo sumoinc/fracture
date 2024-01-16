7c7388523ddd299d3f268d1b7403ce53c7bb1859
Cameron Childress <cameronc@sumoc.com>
2024-01-16 13:06:18 -0500

    wip

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,7 +13,6 @@ const project = new PackageProject({
   authorOrganization: true,
   authorEmail: authorAddress,
   releaseToNpm: true,
-  // projenrcTs: true,
 
   // use the default Apache 2 license
   licensed: true,
@@ -40,62 +39,6 @@ project.addPackageIgnore("/sites");
 
 // dependancies fracture needs
 project.addDeps("change-case", "projen", "type-fest");
-
-/*******************************************************************************
- *
- * Dogfooding for local development and documantation site.
- *
- ******************************************************************************/
-// // build out documentation site
-// const site = new VitePressSite({
-//   parent: project,
-//   name: "docs",
-// });
-
-/*******************************************************************************
- * NETLIFY DEPLOYMENT TARGET
- ******************************************************************************/
-// const netlifyTarget = new NetlifyEnvironment(project, {
-//   name: "netlify",
-//   siteId: "e69db060-d613-414c-9964-4a5a5e0e32ea",
-// });
-
-// // deployment target for docs
-// site.deploy({
-//   branchPrefix: "main",
-//   environment: netlifyTarget,
-// });
-
-/*******************************************************************************
- * AWS SERVICE
- ******************************************************************************/
-
-/*
-const service = new DataService({
-  parent: project,
-  name: "my-service",
-  resourceOptions: [
-    {
-      name: "widget",
-      attributeOptions: [
-        {
-          name: "name",
-          shortName: "n",
-        },
-      ],
-    },
-  ],
-});
-*/
-// kitchen sink test / demo
-// new KitchenSink(service);
-
-/*
-const awsTarget = new AwsEnvironment(project, {
-  name: "hosting",
-  accountNumber: "726654216209",
-});
-*/
 
 // generate
 project.synth();
