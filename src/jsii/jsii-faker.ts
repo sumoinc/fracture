763a4791b96d721c40436549f717ad6ae20ebbaf
Cameron Childress <cameronc@sumoc.com>
2024-01-18 17:12:44 -0500

    test

--- src/jsii/jsii-faker.ts
+++ src/jsii/jsii-faker.ts
@@ -41,7 +41,112 @@ export class JsiiFaker extends Component {
 
     new JsonFile(project, ".jsii", {
       obj: () => {
-        return { name: "@sumoc/fracture", types: this._types };
+        return {
+          author: {
+            name: "me",
+            roles: ["author"],
+            url: "https://github.com/sumoinc",
+          },
+          dependencies: {
+            projen: "^0.79.2",
+          },
+          dependencyClosure: {
+            constructs: {
+              targets: {
+                dotnet: {
+                  namespace: "Constructs",
+                  packageId: "Constructs",
+                },
+                go: {
+                  moduleName: "github.com/aws/constructs-go",
+                },
+                java: {
+                  maven: {
+                    artifactId: "constructs",
+                    groupId: "software.constructs",
+                  },
+                  package: "software.constructs",
+                },
+                js: {
+                  npm: "constructs",
+                },
+                python: {
+                  distName: "constructs",
+                  module: "constructs",
+                },
+              },
+            },
+            projen: {
+              submodules: {
+                "projen.awscdk": {},
+                "projen.build": {},
+                "projen.cdk": {},
+                "projen.cdk8s": {},
+                "projen.cdktf": {},
+                "projen.circleci": {},
+                "projen.github": {},
+                "projen.github.workflows": {},
+                "projen.gitlab": {},
+                "projen.java": {},
+                "projen.javascript": {},
+                "projen.python": {},
+                "projen.release": {},
+                "projen.typescript": {},
+                "projen.vscode": {},
+                "projen.web": {},
+              },
+              targets: {
+                go: {
+                  moduleName: "github.com/projen/projen-go",
+                },
+                java: {
+                  maven: {
+                    artifactId: "projen",
+                    groupId: "io.github.cdklabs",
+                  },
+                  package: "io.github.cdklabs.projen",
+                },
+                js: {
+                  npm: "projen",
+                },
+                python: {
+                  distName: "projen",
+                  module: "projen",
+                },
+              },
+            },
+          },
+          description: "@sumoc/fracture",
+          docs: {
+            stability: "stable",
+          },
+          homepage: "https://github.com/sumoinc/fracture",
+          jsiiVersion: "5.1.12 (build 0675712)",
+          license: "Apache-2.0",
+          metadata: {
+            jsii: {
+              pacmak: {
+                hasDefaultInterfaces: true,
+              },
+            },
+            tscRootDir: "src",
+          },
+          name: "@sumoc/fracture",
+          readme: {
+            markdown: "# replace this",
+          },
+          repository: {
+            type: "git",
+            url: "git@github.com:sumoinc/fracture.git",
+          },
+          schema: "jsii/0.10.0",
+          targets: {
+            js: {
+              npm: "@sumoc/fracture",
+            },
+          },
+          types: this._types,
+        };
       },
     });
   }
